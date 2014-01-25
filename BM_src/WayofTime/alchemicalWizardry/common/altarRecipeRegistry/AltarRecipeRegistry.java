639d9fc87c05a0224cb46a6acd98b199fa63ba79
WayofTime <wtime@live.ca>
2014-01-25 17:11:24 -0500

    Alchemy recipes finished

--- BM_src/WayofTime/alchemicalWizardry/common/altarRecipeRegistry/AltarRecipeRegistry.java
+++ BM_src/WayofTime/alchemicalWizardry/common/altarRecipeRegistry/AltarRecipeRegistry.java
@@ -3,6 +3,8 @@ package WayofTime.alchemicalWizardry.common.altarRecipeRegistry;
 import java.util.LinkedList;
 import java.util.List;
 
+import WayofTime.alchemicalWizardry.AlchemicalWizardry;
+import WayofTime.alchemicalWizardry.ModBlocks;
 import WayofTime.alchemicalWizardry.ModItems;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
@@ -26,20 +28,33 @@ public class AltarRecipeRegistry
 	{
 		registerAltarRecipe(new ItemStack(ModItems.weakBloodOrb), new ItemStack(Item.diamond),1,2000,2,1,false);
 		registerAltarRecipe(new ItemStack(ModItems.apprenticeBloodOrb), new ItemStack(Item.emerald),2,5000,5,5,false);
-		registerAltarRecipe(new ItemStack(ModItems.magicianBloodOrb), new ItemStack(Block.blockGold),3,5000,5,5,false);
+		registerAltarRecipe(new ItemStack(ModItems.magicianBloodOrb), new ItemStack(Block.blockGold),3,25000,20,20,false);
 		registerAltarRecipe(new ItemStack(ModItems.masterBloodOrb), new ItemStack(ModItems.weakBloodShard),4,40000,30,50,false);
-		registerAltarRecipe(new ItemStack(ModItems.archmageBloodOrb), new ItemStack(ModItems.demonBloodShard),5,50000,5,5,false);
+		registerAltarRecipe(new ItemStack(ModItems.archmageBloodOrb), new ItemStack(ModItems.demonBloodShard),5,75000,50,100,false);
 
 		registerAltarOrbRecipe(new ItemStack(ModItems.weakBloodOrb),1,2);
 		registerAltarOrbRecipe(new ItemStack(ModItems.apprenticeBloodOrb),2,5);
-		registerAltarOrbRecipe(new ItemStack(ModItems.magicianBloodOrb),3,5);
+		registerAltarOrbRecipe(new ItemStack(ModItems.magicianBloodOrb),3,15);
 		registerAltarOrbRecipe(new ItemStack(ModItems.masterBloodOrb),4,25);
 		registerAltarOrbRecipe(new ItemStack(ModItems.archmageBloodOrb),5,50);
 		
 		registerAltarRecipe(new ItemStack(ModItems.telepositionFocus), new ItemStack(Item.enderPearl),4,2000,10,10,false);
 		registerAltarRecipe(new ItemStack(ModItems.enhancedTelepositionFocus), new ItemStack(ModItems.telepositionFocus),4,10000,25,15,false);
 		registerAltarRecipe(new ItemStack(ModItems.imbuedSlate), new ItemStack(ModItems.imbuedSlate),4,15000,20,20,false);
-
+		registerAltarRecipe(new ItemStack(ModItems.duskScribeTool), new ItemStack(Block.coalBlock),4,2000,20,10,false);
+		registerAltarRecipe(new ItemStack(ModBlocks.bloodSocket), new ItemStack(ModBlocks.emptySocket),3,30000,40,10,false);
+		registerAltarRecipe(new ItemStack(ModItems.earthScribeTool), new ItemStack(Block.obsidian),3,1000,5,5,false);
+		registerAltarRecipe(new ItemStack(ModItems.waterScribeTool), new ItemStack(Block.blockLapis),3,1000,5,5,false);
+		registerAltarRecipe(new ItemStack(ModItems.blankSpell), new ItemStack(Block.glass),2,1000,5,5,false);
+		registerAltarRecipe(new ItemStack(ModItems.blankSlate), new ItemStack(Block.stone),1,1000,5,5,false);
+		registerAltarRecipe(new ItemStack(ModItems.activationCrystal), new ItemStack(ModItems.lavaCrystal),3,10000,20,10,false);
+		registerAltarRecipe(new ItemStack(ModItems.fireScribeTool), new ItemStack(Item.magmaCream),3,1000,5,5,false);
+		registerAltarRecipe(new ItemStack(ModItems.airScribeTool), new ItemStack(Item.ghastTear),3,1000,5,5,false);
+		registerAltarRecipe(new ItemStack(ModItems.imbuedSlate), new ItemStack(ModItems.reinforcedSlate),3,5000,15,10,false);
+		registerAltarRecipe(new ItemStack(ModItems.daggerOfSacrifice), new ItemStack(Item.swordIron),2,3000,5,5,false);
+		registerAltarRecipe(new ItemStack(ModItems.alchemyFlask), new ItemStack(Item.glassBottle),2,2000,5,5,false);
+		registerAltarRecipe(new ItemStack(ModItems.reinforcedSlate), new ItemStack(ModItems.blankSlate),2,2000,5,5,false);
+		registerAltarRecipe(new ItemStack(AlchemicalWizardry.bucketLife), new ItemStack(Item.bucketEmpty),1,1000,5,0,false);
 	}
 	
 	public static boolean isRequiredItemValid(ItemStack testItem, int currentTierAltar)
