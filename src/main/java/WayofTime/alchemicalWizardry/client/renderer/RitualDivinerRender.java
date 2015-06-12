8d2ab7aa7af774ff5d9584490863187bdabf569c
Arcaratus <byou64@gmail.com>
2015-06-12 14:53:28 -0400

    Small stuff

--- src/main/java/WayofTime/alchemicalWizardry/client/renderer/RitualDivinerRender.java
+++ src/main/java/WayofTime/alchemicalWizardry/client/renderer/RitualDivinerRender.java
@@ -1,5 +1,7 @@
 package WayofTime.alchemicalWizardry.client.renderer;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import org.lwjgl.opengl.GL11;
 
 import WayofTime.alchemicalWizardry.ModBlocks;
@@ -21,6 +23,7 @@ import net.minecraftforge.client.event.RenderWorldLastEvent;
  *  Created in Scala by Alex-Hawks
  *  Translated and implemented by Arcaratus
  */
+@SideOnly(Side.CLIENT)
 public class RitualDivinerRender
 {
     @SubscribeEvent
