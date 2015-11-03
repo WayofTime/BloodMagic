75143520ed3d6242d695cac2e260d42f69ffe8db
Arcaratus <byou64@gmail.com>
2015-11-02 21:30:28 -0500

    Pre-emptive things

--- src/main/java/WayofTime/bloodmagic/api/altar/AltarUpgrade.java
+++ src/main/java/WayofTime/bloodmagic/api/altar/AltarUpgrade.java
@@ -3,18 +3,77 @@ package WayofTime.bloodmagic.api.altar;
 import lombok.Getter;
 
 @Getter
-public class AltarUpgrade {
-
+public class AltarUpgrade
+{
     private int speedCount;
+    private int efficiencyCount;
+    private int sacrificeCount;
+    private int selfSacrificeCount;
+    private int displacementCount;
+    private int capacityCount;
+    private int orbCapacityCount;
+    private int betterCapacityCount;
+    private int accelerationCount;
 
-    public AltarUpgrade() {
+    public AltarUpgrade()
+    {
 
     }
 
     // Adders
 
-    public AltarUpgrade addSpeed() {
+    public AltarUpgrade addSpeed()
+    {
         speedCount++;
         return this;
     }
+
+    public AltarUpgrade addEfficiency()
+    {
+        efficiencyCount++;
+        return this;
+    }
+
+    public AltarUpgrade addSacrifice()
+    {
+        sacrificeCount++;
+        return this;
+    }
+
+    public AltarUpgrade addSelfSacrifice()
+    {
+        selfSacrificeCount++;
+        return this;
+    }
+
+    public AltarUpgrade addDisplacement()
+    {
+        displacementCount++;
+        return this;
+    }
+
+    public AltarUpgrade addCapacity()
+    {
+        capacityCount++;
+        return this;
+    }
+
+    public AltarUpgrade addOrbCapacity()
+    {
+        orbCapacityCount++;
+        return this;
+    }
+
+    public AltarUpgrade addBetterCapacity()
+    {
+        betterCapacityCount++;
+        return this;
+    }
+
+    public AltarUpgrade addAcceleration()
+    {
+        accelerationCount++;
+        return this;
+    }
+
 }
--- src/main/java/WayofTime/bloodmagic/api/altar/EnumAltarTier.java
+++ src/main/java/WayofTime/bloodmagic/api/altar/EnumAltarTier.java
@@ -14,28 +14,28 @@ public enum EnumAltarTier {
     TWO() {
         @Override
         public void buildComponents() {
-            altarComponents.add(new AltarComponent(new BlockPos(-1, -1, -1), ModBlocks.rune).setBloodRune());
-            altarComponents.add(new AltarComponent(new BlockPos(0, -1, -1), ModBlocks.rune).setUpgradeSlot().setBloodRune());
-            altarComponents.add(new AltarComponent(new BlockPos(1, -1, -1), ModBlocks.rune).setBloodRune());
-            altarComponents.add(new AltarComponent(new BlockPos(-1, -1, 0), ModBlocks.rune).setUpgradeSlot().setBloodRune());
-            altarComponents.add(new AltarComponent(new BlockPos(1, -1, 0), ModBlocks.rune).setUpgradeSlot().setBloodRune());
-            altarComponents.add(new AltarComponent(new BlockPos(-1, -1, 1), ModBlocks.rune).setBloodRune());
-            altarComponents.add(new AltarComponent(new BlockPos(0, -1, 1), ModBlocks.rune).setUpgradeSlot().setBloodRune());
-            altarComponents.add(new AltarComponent(new BlockPos(1, -1, 1), ModBlocks.rune).setBloodRune());
+            altarComponents.add(new AltarComponent(new BlockPos(-1, -1, -1), ModBlocks.blood_rune).setBloodRune());
+            altarComponents.add(new AltarComponent(new BlockPos(0, -1, -1), ModBlocks.blood_rune).setUpgradeSlot().setBloodRune());
+            altarComponents.add(new AltarComponent(new BlockPos(1, -1, -1), ModBlocks.blood_rune).setBloodRune());
+            altarComponents.add(new AltarComponent(new BlockPos(-1, -1, 0), ModBlocks.blood_rune).setUpgradeSlot().setBloodRune());
+            altarComponents.add(new AltarComponent(new BlockPos(1, -1, 0), ModBlocks.blood_rune).setUpgradeSlot().setBloodRune());
+            altarComponents.add(new AltarComponent(new BlockPos(-1, -1, 1), ModBlocks.blood_rune).setBloodRune());
+            altarComponents.add(new AltarComponent(new BlockPos(0, -1, 1), ModBlocks.blood_rune).setUpgradeSlot().setBloodRune());
+            altarComponents.add(new AltarComponent(new BlockPos(1, -1, 1), ModBlocks.blood_rune).setBloodRune());
         }
     },
     THREE() {
         @Override
         public void buildComponents() {
             altarComponents.addAll(TWO.getAltarComponents());
-            altarComponents.add(new AltarComponent(new BlockPos(-1, -1, -1), ModBlocks.rune).setUpgradeSlot().setBloodRune());
-            altarComponents.add(new AltarComponent(new BlockPos(0, -1, -1), ModBlocks.rune).setUpgradeSlot().setBloodRune());
-            altarComponents.add(new AltarComponent(new BlockPos(1, -1, -1), ModBlocks.rune).setUpgradeSlot().setBloodRune());
-            altarComponents.add(new AltarComponent(new BlockPos(-1, -1, 0), ModBlocks.rune).setUpgradeSlot().setBloodRune());
-            altarComponents.add(new AltarComponent(new BlockPos(1, -1, 0), ModBlocks.rune).setUpgradeSlot().setBloodRune());
-            altarComponents.add(new AltarComponent(new BlockPos(-1, -1, 1), ModBlocks.rune).setUpgradeSlot().setBloodRune());
-            altarComponents.add(new AltarComponent(new BlockPos(0, -1, 1), ModBlocks.rune).setUpgradeSlot().setBloodRune());
-            altarComponents.add(new AltarComponent(new BlockPos(1, -1, 1), ModBlocks.rune).setUpgradeSlot().setBloodRune());
+            altarComponents.add(new AltarComponent(new BlockPos(-1, -1, -1), ModBlocks.blood_rune).setUpgradeSlot().setBloodRune());
+            altarComponents.add(new AltarComponent(new BlockPos(0, -1, -1), ModBlocks.blood_rune).setUpgradeSlot().setBloodRune());
+            altarComponents.add(new AltarComponent(new BlockPos(1, -1, -1), ModBlocks.blood_rune).setUpgradeSlot().setBloodRune());
+            altarComponents.add(new AltarComponent(new BlockPos(-1, -1, 0), ModBlocks.blood_rune).setUpgradeSlot().setBloodRune());
+            altarComponents.add(new AltarComponent(new BlockPos(1, -1, 0), ModBlocks.blood_rune).setUpgradeSlot().setBloodRune());
+            altarComponents.add(new AltarComponent(new BlockPos(-1, -1, 1), ModBlocks.blood_rune).setUpgradeSlot().setBloodRune());
+            altarComponents.add(new AltarComponent(new BlockPos(0, -1, 1), ModBlocks.blood_rune).setUpgradeSlot().setBloodRune());
+            altarComponents.add(new AltarComponent(new BlockPos(1, -1, 1), ModBlocks.blood_rune).setUpgradeSlot().setBloodRune());
             altarComponents.add(new AltarComponent(new BlockPos(-3, -1, -3)));
             altarComponents.add(new AltarComponent(new BlockPos(-3, 0, -3)));
             altarComponents.add(new AltarComponent(new BlockPos(3, -1, -3)));
@@ -50,10 +50,10 @@ public enum EnumAltarTier {
             altarComponents.add(new AltarComponent(new BlockPos(3, 1, 3), Blocks.glowstone));
 
             for (int i = -2; i <= 2; i++) {
-                altarComponents.add(new AltarComponent(new BlockPos(3, -2, i), ModBlocks.rune).setUpgradeSlot().setBloodRune());
-                altarComponents.add(new AltarComponent(new BlockPos(-3, -2, i), ModBlocks.rune).setUpgradeSlot().setBloodRune());
-                altarComponents.add(new AltarComponent(new BlockPos(i, -2, 3), ModBlocks.rune).setUpgradeSlot().setBloodRune());
-                altarComponents.add(new AltarComponent(new BlockPos(i, -2, -3), ModBlocks.rune).setUpgradeSlot().setBloodRune());
+                altarComponents.add(new AltarComponent(new BlockPos(3, -2, i), ModBlocks.blood_rune).setUpgradeSlot().setBloodRune());
+                altarComponents.add(new AltarComponent(new BlockPos(-3, -2, i), ModBlocks.blood_rune).setUpgradeSlot().setBloodRune());
+                altarComponents.add(new AltarComponent(new BlockPos(i, -2, 3), ModBlocks.blood_rune).setUpgradeSlot().setBloodRune());
+                altarComponents.add(new AltarComponent(new BlockPos(i, -2, -3), ModBlocks.blood_rune).setUpgradeSlot().setBloodRune());
             }
         }
     },
@@ -63,10 +63,10 @@ public enum EnumAltarTier {
             altarComponents.addAll(THREE.getAltarComponents());
 
             for (int i = -3; i <= 3; i++) {
-                altarComponents.add(new AltarComponent(new BlockPos(5, -3, i), ModBlocks.rune).setUpgradeSlot().setBloodRune());
-                altarComponents.add(new AltarComponent(new BlockPos(-5, -3, i), ModBlocks.rune).setUpgradeSlot().setBloodRune());
-                altarComponents.add(new AltarComponent(new BlockPos(i, -3, 5), ModBlocks.rune).setUpgradeSlot().setBloodRune());
-                altarComponents.add(new AltarComponent(new BlockPos(i, -3, -5), ModBlocks.rune).setUpgradeSlot().setBloodRune());
+                altarComponents.add(new AltarComponent(new BlockPos(5, -3, i), ModBlocks.blood_rune).setUpgradeSlot().setBloodRune());
+                altarComponents.add(new AltarComponent(new BlockPos(-5, -3, i), ModBlocks.blood_rune).setUpgradeSlot().setBloodRune());
+                altarComponents.add(new AltarComponent(new BlockPos(i, -3, 5), ModBlocks.blood_rune).setUpgradeSlot().setBloodRune());
+                altarComponents.add(new AltarComponent(new BlockPos(i, -3, -5), ModBlocks.blood_rune).setUpgradeSlot().setBloodRune());
             }
 
             for (int i = -2; i <= 1; i++) {
@@ -92,10 +92,10 @@ public enum EnumAltarTier {
             altarComponents.add(new AltarComponent(new BlockPos(8, -3, 8), Blocks.beacon));
 
             for (int i = -6; i <= 6; i++) {
-                altarComponents.add(new AltarComponent(new BlockPos(8, -4, i), ModBlocks.rune).setUpgradeSlot().setBloodRune());
-                altarComponents.add(new AltarComponent(new BlockPos(-8, -4, i), ModBlocks.rune).setUpgradeSlot().setBloodRune());
-                altarComponents.add(new AltarComponent(new BlockPos(i, -4, 8), ModBlocks.rune).setUpgradeSlot().setBloodRune());
-                altarComponents.add(new AltarComponent(new BlockPos(i, -4, -8), ModBlocks.rune).setUpgradeSlot().setBloodRune());
+                altarComponents.add(new AltarComponent(new BlockPos(8, -4, i), ModBlocks.blood_rune).setUpgradeSlot().setBloodRune());
+                altarComponents.add(new AltarComponent(new BlockPos(-8, -4, i), ModBlocks.blood_rune).setUpgradeSlot().setBloodRune());
+                altarComponents.add(new AltarComponent(new BlockPos(i, -4, 8), ModBlocks.blood_rune).setUpgradeSlot().setBloodRune());
+                altarComponents.add(new AltarComponent(new BlockPos(i, -4, -8), ModBlocks.blood_rune).setUpgradeSlot().setBloodRune());
             }
         }
     },
@@ -117,10 +117,10 @@ public enum EnumAltarTier {
             altarComponents.add(new AltarComponent(new BlockPos(-11, 3, 11), ModBlocks.crystal));
 
             for (int i = -9; i <= 9; i++) {
-                altarComponents.add(new AltarComponent(new BlockPos(11, -5, i), ModBlocks.rune).setUpgradeSlot().setBloodRune());
-                altarComponents.add(new AltarComponent(new BlockPos(-11, -5, i), ModBlocks.rune).setUpgradeSlot().setBloodRune());
-                altarComponents.add(new AltarComponent(new BlockPos(i, -5, 11), ModBlocks.rune).setUpgradeSlot().setBloodRune());
-                altarComponents.add(new AltarComponent(new BlockPos(i, -5, -11), ModBlocks.rune).setUpgradeSlot().setBloodRune());
+                altarComponents.add(new AltarComponent(new BlockPos(11, -5, i), ModBlocks.blood_rune).setUpgradeSlot().setBloodRune());
+                altarComponents.add(new AltarComponent(new BlockPos(-11, -5, i), ModBlocks.blood_rune).setUpgradeSlot().setBloodRune());
+                altarComponents.add(new AltarComponent(new BlockPos(i, -5, 11), ModBlocks.blood_rune).setUpgradeSlot().setBloodRune());
+                altarComponents.add(new AltarComponent(new BlockPos(i, -5, -11), ModBlocks.blood_rune).setUpgradeSlot().setBloodRune());
             }
         }
     };
