ebb8f7e9805948c5596e6b8062b0e4faab369d94
Arcaratus <byou64@gmail.com>
2015-11-04 17:22:02 -0500

    Does this work

--- src/main/java/WayofTime/bloodmagic/api/util/helper/IncenseHelper.java
+++ src/main/java/WayofTime/bloodmagic/api/util/helper/IncenseHelper.java
@@ -4,19 +4,16 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 
 public class IncenseHelper {
-    public static float getCurrentIncense(EntityPlayer player)
-    {
+    public static float getCurrentIncense(EntityPlayer player) {
         NBTTagCompound data = player.getEntityData();
-        if(data.hasKey("BM:CurrentIncense"))
-        {
+        if (data.hasKey("BM:CurrentIncense")) {
             return data.getFloat("BM:CurrentIncense");
         }
 
         return 0;
     }
 
-    public static void setCurrentIncense(EntityPlayer player, float amount)
-    {
+    public static void setCurrentIncense(EntityPlayer player, float amount) {
         NBTTagCompound data = player.getEntityData();
         data.setFloat("BM:CurrentIncense", amount);
     }
--- src/main/java/WayofTime/bloodmagic/api/util/helper/PlayerSacrificeHelper.java
+++ src/main/java/WayofTime/bloodmagic/api/util/helper/PlayerSacrificeHelper.java
@@ -12,21 +12,18 @@ public class PlayerSacrificeHelper {
     public static float scalingOfSacrifice = 0.001f;
     public static int soulFrayDuration = 400;
     public static Potion soulFrayId;
-    public static float getPlayerIncense(EntityPlayer player)
-    {
+
+    public static float getPlayerIncense(EntityPlayer player) {
         return IncenseHelper.getCurrentIncense(player);
     }
 
-    public static void setPlayerIncense(EntityPlayer player, float amount)
-    {
+    public static void setPlayerIncense(EntityPlayer player, float amount) {
         IncenseHelper.setCurrentIncense(player, amount);
     }
 
-    public static boolean incrementIncense(EntityPlayer player, float min, float max, float increment)
-    {
+    public static boolean incrementIncense(EntityPlayer player, float min, float max, float increment) {
         float amount = getPlayerIncense(player);
-        if(amount < min || amount >= max)
-        {
+        if (amount < min || amount >= max) {
             return false;
         }
 
@@ -38,27 +35,22 @@ public class PlayerSacrificeHelper {
         return true;
     }
 
-    public static boolean sacrificePlayerHealth(EntityPlayer player)
-    {
-        if(player.isPotionActive(soulFrayId))
-        {
+    public static boolean sacrificePlayerHealth(EntityPlayer player) {
+        if (player.isPotionActive(soulFrayId)) {
             return false;
         }
 
         float amount = getPlayerIncense(player);
 
-        if(amount >= 0)
-        {
+        if (amount >= 0) {
             float health = player.getHealth();
             float maxHealth = player.getMaxHealth();
 
-            if(health > maxHealth/10.0)
-            {
-                float sacrificedHealth = health - maxHealth/10.0f;
+            if (health > maxHealth / 10.0) {
+                float sacrificedHealth = health - maxHealth / 10.0f;
 
-                if(findAndFillAltar(player.getEntityWorld(), player, (int)(sacrificedHealth * 100f * getModifier(amount))))
-                {
-                    player.setHealth(maxHealth/10.0f);
+                if (findAndFillAltar(player.getEntityWorld(), player, (int) (sacrificedHealth * 100f * getModifier(amount)))) {
+                    player.setHealth(maxHealth / 10.0f);
                     setPlayerIncense(player, 0);
                     player.addPotionEffect(new PotionEffect(soulFrayId.id, soulFrayDuration));
 
@@ -70,20 +62,17 @@ public class PlayerSacrificeHelper {
         return false;
     }
 
-    public static float getModifier(float amount)
-    {
-        return 1 + amount*scalingOfSacrifice;
+    public static float getModifier(float amount) {
+        return 1 + amount * scalingOfSacrifice;
     }
 
-    public static boolean findAndFillAltar(World world, EntityPlayer player, int amount)
-    {
+    public static boolean findAndFillAltar(World world, EntityPlayer player, int amount) {
         int posX = (int) Math.round(player.posX - 0.5f);
         int posY = (int) player.posY;
         int posZ = (int) Math.round(player.posZ - 0.5f);
         IBloodAltar altarEntity = getAltar(world, new BlockPos(posX, posY, posZ));
 
-        if (altarEntity == null)
-        {
+        if (altarEntity == null) {
             return false;
         }
 
@@ -93,21 +82,16 @@ public class PlayerSacrificeHelper {
         return true;
     }
 
-    public static IBloodAltar getAltar(World world, BlockPos blockPos)
-    {
+    public static IBloodAltar getAltar(World world, BlockPos blockPos) {
         TileEntity tileEntity;
 
-        for (int i = -2; i <= 2; i++)
-        {
-            for (int j = -2; j <= 2; j++)
-            {
-                for (int k = -2; k <= 1; k++)
-                {
+        for (int i = -2; i <= 2; i++) {
+            for (int j = -2; j <= 2; j++) {
+                for (int k = -2; k <= 1; k++) {
                     tileEntity = world.getTileEntity(new BlockPos(i + blockPos.getX(), k + blockPos.getY(), j + blockPos.getZ()));
 
-                    if(tileEntity instanceof IBloodAltar)
-                    {
-                        return (IBloodAltar)tileEntity;
+                    if (tileEntity instanceof IBloodAltar) {
+                        return (IBloodAltar) tileEntity;
                     }
                 }
             }
