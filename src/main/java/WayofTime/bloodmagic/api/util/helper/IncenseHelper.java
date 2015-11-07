806f291d024e1ded7d849d98504fcf3b0279d6fe
Arcaratus <byou64@gmail.com>
2015-11-07 11:54:36 -0500

    Fix spelling of 'incense'

--- src/main/java/WayofTime/bloodmagic/api/util/helper/IncenseHelper.java
+++ src/main/java/WayofTime/bloodmagic/api/util/helper/IncenseHelper.java
@@ -7,8 +7,8 @@ import net.minecraft.nbt.NBTTagCompound;
 public class IncenseHelper {
     public static float getCurrentIncense(EntityPlayer player) {
         NBTTagCompound data = player.getEntityData();
-        if (data.hasKey(NBTHolder.NBT_CURRENT_INSENCE)) {
-            return data.getFloat(NBTHolder.NBT_CURRENT_INSENCE);
+        if (data.hasKey(NBTHolder.NBT_CURRENT_INCENSE)) {
+            return data.getFloat(NBTHolder.NBT_CURRENT_INCENSE);
         }
 
         return 0;
@@ -16,6 +16,6 @@ public class IncenseHelper {
 
     public static void setCurrentIncense(EntityPlayer player, float amount) {
         NBTTagCompound data = player.getEntityData();
-        data.setFloat(NBTHolder.NBT_CURRENT_INSENCE, amount);
+        data.setFloat(NBTHolder.NBT_CURRENT_INCENSE, amount);
     }
 }
