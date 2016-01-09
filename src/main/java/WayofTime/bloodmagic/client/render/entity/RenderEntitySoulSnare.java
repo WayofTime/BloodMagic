1171861785bb192d02f66271e3f78a5b1665c082
Nick <speedynutty68@gmail.com>
2016-01-08 16:36:07 -0800

    Fix javadoc compile issues

--- src/main/java/WayofTime/bloodmagic/client/render/entity/RenderEntitySoulSnare.java
+++ src/main/java/WayofTime/bloodmagic/client/render/entity/RenderEntitySoulSnare.java
@@ -23,14 +23,6 @@ public class RenderEntitySoulSnare extends Render<EntitySoulSnare>
         this.field_177083_e = p_i46137_3_;
     }
 
-    /**
-     * Actually renders the given argument. This is a synthetic bridge method,
-     * always casting down its argument and then handing it off to a worker
-     * function which does the actual work. In all probabilty, the class Render
-     * is generic (Render<T extends Entity>) and this method has signature
-     * public void func_76986_a(T entity, double d, double d1, double d2, float
-     * f, float f1). But JAD is pre 1.5 so doe
-     */
     public void doRender(EntitySoulSnare entity, double x, double y, double z, float entityYaw, float partialTicks)
     {
         GlStateManager.pushMatrix();
@@ -51,10 +43,6 @@ public class RenderEntitySoulSnare extends Render<EntitySoulSnare>
         return new ItemStack(this.field_177084_a, 1, 0);
     }
 
-    /**
-     * Returns the location of an entity's texture. Doesn't seem to be called
-     * unless you call Render.bindEntityTexture.
-     */
     protected ResourceLocation getEntityTexture(EntitySoulSnare entity)
     {
         return TextureMap.locationBlocksTexture;
