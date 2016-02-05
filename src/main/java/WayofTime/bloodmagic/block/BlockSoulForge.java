10b58ee7ad26cc0fa17dbe86181dfd029b2887f5
Nick <speedynutty68@gmail.com>
2016-02-05 15:17:43 -0800

    Update models. Change bounding box of Hellfire Forge

--- src/main/java/WayofTime/bloodmagic/block/BlockSoulForge.java
+++ src/main/java/WayofTime/bloodmagic/block/BlockSoulForge.java
@@ -1,11 +1,9 @@
 package WayofTime.bloodmagic.block;
 
-import WayofTime.bloodmagic.tile.TileAltar;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
@@ -13,7 +11,6 @@ import net.minecraft.world.World;
 import WayofTime.bloodmagic.BloodMagic;
 import WayofTime.bloodmagic.api.Constants;
 import WayofTime.bloodmagic.tile.TileSoulForge;
-import WayofTime.bloodmagic.tile.TileTeleposer;
 
 public class BlockSoulForge extends BlockContainer
 {
@@ -28,6 +25,8 @@ public class BlockSoulForge extends BlockContainer
         setStepSound(soundTypeMetal);
         setHarvestLevel("pickaxe", 1);
         setCreativeTab(BloodMagic.tabBloodMagic);
+
+        setBlockBounds(0.06F, 0.0F, 0.06F, 0.94F, 0.75F, 0.94F);
     }
 
     @Override
@@ -58,9 +57,7 @@ public class BlockSoulForge extends BlockContainer
     public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumFacing side, float hitX, float hitY, float hitZ)
     {
         if (world.getTileEntity(pos) instanceof TileSoulForge)
-        {
             player.openGui(BloodMagic.instance, Constants.Gui.SOUL_FORGE_GUI, world, pos.getX(), pos.getY(), pos.getZ());
-        }
 
         return true;
     }
