e89451ed3663b01676398fd4bfb5795831b282c0
Arcaratus <byou64@gmail.com>
2016-02-27 11:38:40 -0500

    Routing node cleanup

--- src/main/java/WayofTime/bloodmagic/tile/container/ContainerSoulForge.java
+++ src/main/java/WayofTime/bloodmagic/tile/container/ContainerSoulForge.java
@@ -42,7 +42,7 @@ public class ContainerSoulForge extends Container
     public ItemStack transferStackInSlot(EntityPlayer playerIn, int index)
     {
         ItemStack itemstack = null;
-        Slot slot = (Slot) this.inventorySlots.get(index);
+        Slot slot = this.inventorySlots.get(index);
 
         if (slot != null && slot.getHasStack())
         {
@@ -76,7 +76,7 @@ public class ContainerSoulForge extends Container
 
             if (itemstack1.stackSize == 0)
             {
-                slot.putStack((ItemStack) null);
+                slot.putStack(null);
             } else
             {
                 slot.onSlotChanged();
