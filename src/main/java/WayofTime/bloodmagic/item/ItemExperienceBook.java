1d14692a931692eaca27982d2bfa6b86a60f32b1
WayofTime <wtime@live.ca>
2016-04-02 16:42:04 -0400

    Slowly working on an experience book for the mod. Changed Curse building to Beta

--- src/main/java/WayofTime/bloodmagic/item/ItemExperienceBook.java
+++ src/main/java/WayofTime/bloodmagic/item/ItemExperienceBook.java
@@ -6,6 +6,7 @@ import java.util.List;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
@@ -19,6 +20,7 @@ import org.apache.commons.lang3.tuple.Pair;
 
 import WayofTime.bloodmagic.BloodMagic;
 import WayofTime.bloodmagic.api.Constants;
+import WayofTime.bloodmagic.api.util.helper.NBTHelper;
 import WayofTime.bloodmagic.client.IVariantProvider;
 import WayofTime.bloodmagic.util.helper.TextHelper;
 
@@ -37,6 +39,8 @@ public class ItemExperienceBook extends Item implements IVariantProvider
     public void addInformation(ItemStack stack, EntityPlayer player, List<String> tooltip, boolean advanced)
     {
         tooltip.add(TextHelper.localizeEffect("tooltip.BloodMagic.experienceTome"));
+
+        tooltip.add(TextHelper.localizeEffect("tooltip.BloodMagic.experienceTome.exp", getStoredExperience(stack)));
     }
 
     @Override
@@ -52,5 +56,47 @@ public class ItemExperienceBook extends Item implements IVariantProvider
         List<Pair<Integer, String>> ret = new ArrayList<Pair<Integer, String>>();
         ret.add(new ImmutablePair<Integer, String>(0, "type=experiencetome"));
         return ret;
+    }
+
+    public static void setStoredExperience(ItemStack stack, double exp)
+    {
+        NBTHelper.checkNBT(stack);
+
+        NBTTagCompound tag = stack.getTagCompound();
+
+        tag.setDouble("experience", exp);
+    }
+
+    public static double getStoredExperience(ItemStack stack)
+    {
+        NBTHelper.checkNBT(stack);
+
+        NBTTagCompound tag = stack.getTagCompound();
+
+        return tag.getDouble("experience");
+    }
+
+    public static void addExperience(ItemStack stack, double exp)
+    {
+        setStoredExperience(stack, getStoredExperience(stack) + exp);
+    }
+
+    public static int getExperienceForNextLevel(int currentLevel)
+    {
+        if (currentLevel <= 16)
+        {
+            return 2 * currentLevel + 7;
+        } else if (currentLevel <= 31)
+        {
+            return 5 * currentLevel - 38;
+        } else
+        {
+            return 9 * currentLevel - 158;
+        }
+    }
+
+    public static double getExperienceAcquiredToNext(int currentLevel, double progress)
+    {
+        return progress * getExperienceForNextLevel(currentLevel);
     }
 }
