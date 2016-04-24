b408234ab0bd32ecdef7969ea4efcfd8aa1bc3b1
Nicholas Ignoffo <speedynutty68@gmail.com>
2016-04-24 08:50:10 -0700

    Fix formatting in compression compat

--- src/main/java/WayofTime/bloodmagic/compat/compression/CompatibilityCompression.java
+++ src/main/java/WayofTime/bloodmagic/compat/compression/CompatibilityCompression.java
@@ -5,17 +5,22 @@ import WayofTime.bloodmagic.ConfigHandler;
 import WayofTime.bloodmagic.compat.ICompatibility;
 import WayofTime.bloodmagic.compress.StorageBlockCraftingRecipeAssimilator;
 
-public class CompatibilityCompression implements ICompatibility{
+public class CompatibilityCompression implements ICompatibility
+{
 
     @Override
-    public void loadCompatibility(InitializationPhase phase) {
-        if (phase == InitializationPhase.INIT) {
-            try {
+    public void loadCompatibility(InitializationPhase phase)
+    {
+        if (phase == InitializationPhase.INIT)
+        {
+            try
+            {
                 Class compressionRecipe = Class.forName("temportalist.compression.main.common.recipe.RecipeClassicCompress");
                 Class decompressionRecipe = Class.forName("temportalist.compression.main.common.recipe.RecipeClassicDecompress");
                 StorageBlockCraftingRecipeAssimilator.ignore.add(compressionRecipe);
                 StorageBlockCraftingRecipeAssimilator.ignore.add(decompressionRecipe);
-            } catch (ClassNotFoundException e) {
+            } catch (ClassNotFoundException e)
+            {
                 BloodMagic.instance.getLogger().error("Found mod Compression but did not find the IRecipe classes. Did they get moved?");
                 BloodMagic.instance.getLogger().error(e.getLocalizedMessage());
             }
@@ -23,12 +28,14 @@ public class CompatibilityCompression implements ICompatibility{
     }
 
     @Override
-    public String getModId() {
+    public String getModId()
+    {
         return "compression";
     }
 
     @Override
-    public boolean enableCompat() {
+    public boolean enableCompat()
+    {
         return ConfigHandler.ignoreCompressionSpamAddedByCompression;
     }
 }
