cfce1a4a6c610abf1601ed97abffbedabeea6461
Nicholas Ignoffo <speedynutty68@gmail.com>
2016-05-11 17:40:41 -0700

    Fix spectral blocks being opaque (#716)
    Now you can see through the missing fluids
    
    Also reduced the opacity slightly.

--- src/main/java/WayofTime/bloodmagic/block/BlockSpectral.java
+++ src/main/java/WayofTime/bloodmagic/block/BlockSpectral.java
@@ -9,12 +9,16 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumBlockRenderType;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 
@@ -46,6 +50,12 @@ public class BlockSpectral extends BlockContainer implements IVariantProvider
     }
 
     @Override
+    public boolean isOpaqueCube(IBlockState state)
+    {
+        return false;
+    }
+
+    @Override
     public boolean isFullCube(IBlockState state)
     {
         return false;
@@ -57,10 +67,23 @@ public class BlockSpectral extends BlockContainer implements IVariantProvider
         return false;
     }
 
+    @SideOnly(Side.CLIENT)
+    @Override
+    public BlockRenderLayer getBlockLayer()
+    {
+        return BlockRenderLayer.TRANSLUCENT;
+    }
+
     @Override
     public EnumBlockRenderType getRenderType(IBlockState state)
     {
         return EnumBlockRenderType.MODEL;
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public boolean shouldSideBeRendered(IBlockState state, IBlockAccess world, BlockPos pos, EnumFacing side) {
+        return world.getBlockState(pos.offset(side)) != state || state.getBlock() != this && super.shouldSideBeRendered(state, world, pos, side);
     }
 
     @Override
