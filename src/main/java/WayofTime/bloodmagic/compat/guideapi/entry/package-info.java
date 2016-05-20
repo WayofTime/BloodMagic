c66b99b535a78f5e00443d133a52511430618af9
Nicholas Ignoffo <speedynutty68@gmail.com>
2016-05-19 17:46:11 -0700

    Remove things that shouldn't be there

--- src/main/java/WayofTime/bloodmagic/compat/guideapi/entry/package-info.java
+++
@@ -1,7 +0,0 @@
-@ParametersAreNonnullByDefault
-@MethodsReturnNonnullByDefault
-package WayofTime.bloodmagic.compat.guideapi.entry;
-
-import mcp.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
