ed33ffc23241d9cafe9408a245959be0ebfd38b7
Arcaratus <byou64@gmail.com>
2016-06-17 18:45:39 -0400

    Qiiick

--- src/main/java/WayofTime/bloodmagic/ConfigHandler.java
+++ src/main/java/WayofTime/bloodmagic/ConfigHandler.java
@@ -291,7 +291,7 @@ public class ConfigHandler
         config.addCustomCategoryComment(category, "Client only settings");
         alwaysRenderRoutingLines = config.getBoolean("alwaysRenderRoutingLines", category, false, "Always renders the beams between routing nodes. If false, only renders while a Node Router is being held.");
         invisibleSpectralBlocks = config.get(category, "invisibleSpectralBlocks", true, "Spectral Blocks (Used by the Suppression Sigil to store fluids) will not render at all. If false, a see through texture will render. [default: true]").setRequiresMcRestart(true).getBoolean();
-        sigilHoldingSkipsEmptySlots = config.getBoolean( "sigilHoldingSkipsEmptySlots", category, false, "The Sigil of Holding will skip empty sigil slots if set to true. [default: false]");
+        sigilHoldingSkipsEmptySlots = config.getBoolean( "sigilHoldingSkipsEmptySlots", category, false, "The Sigil of Holding will skip empty sigil slots if set to true.");
 
         category = "Compatibility";
         config.addCustomCategoryComment(category, "Compatibility settings");
