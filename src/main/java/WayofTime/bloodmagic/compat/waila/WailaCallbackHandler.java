a38d9b435b8dbcef8c3c5924bd3f4411923191ad
Nicholas Ignoffo <speedynutty68@gmail.com>
2016-08-14 12:13:19 -0700

    Waila compat for mimic blocks

--- src/main/java/WayofTime/bloodmagic/compat/waila/WailaCallbackHandler.java
+++ src/main/java/WayofTime/bloodmagic/compat/waila/WailaCallbackHandler.java
@@ -1,15 +1,9 @@
 package WayofTime.bloodmagic.compat.waila;
 
+import WayofTime.bloodmagic.block.*;
+import WayofTime.bloodmagic.compat.waila.provider.*;
 import mcp.mobius.waila.api.IWailaRegistrar;
 import WayofTime.bloodmagic.api.Constants;
-import WayofTime.bloodmagic.block.BlockAlchemyArray;
-import WayofTime.bloodmagic.block.BlockAltar;
-import WayofTime.bloodmagic.block.BlockRitualController;
-import WayofTime.bloodmagic.block.BlockTeleposer;
-import WayofTime.bloodmagic.compat.waila.provider.DataProviderAlchemyArray;
-import WayofTime.bloodmagic.compat.waila.provider.DataProviderBloodAltar;
-import WayofTime.bloodmagic.compat.waila.provider.DataProviderRitualController;
-import WayofTime.bloodmagic.compat.waila.provider.DataProviderTeleposer;
 
 public class WailaCallbackHandler
 {
@@ -20,6 +14,7 @@ public class WailaCallbackHandler
         registrar.registerBodyProvider(new DataProviderRitualController(), BlockRitualController.class);
         registrar.registerBodyProvider(new DataProviderAlchemyArray(), BlockAlchemyArray.class);
         registrar.registerStackProvider(new DataProviderAlchemyArray(), BlockAlchemyArray.class);
+        registrar.registerStackProvider(new DataProviderMimic(), BlockMimic.class);
 
         registrar.addConfig(Constants.Mod.MODID, Constants.Compat.WAILA_CONFIG_BYPASS_SNEAK, false);
         registrar.addConfig(Constants.Mod.MODID, Constants.Compat.WAILA_CONFIG_ALTAR, true);
---
+++ src/main/java/WayofTime/bloodmagic/compat/waila/provider/DataProviderMimic.java
@@ -0,0 +1,50 @@
+package WayofTime.bloodmagic.compat.waila.provider;
+
+import WayofTime.bloodmagic.registry.ModBlocks;
+import WayofTime.bloodmagic.tile.TileMimic;
+import mcp.mobius.waila.api.IWailaConfigHandler;
+import mcp.mobius.waila.api.IWailaDataAccessor;
+import mcp.mobius.waila.api.IWailaDataProvider;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import java.util.List;
+
+public class DataProviderMimic implements IWailaDataProvider
+{
+
+    @Override
+    public ItemStack getWailaStack(IWailaDataAccessor accessor, IWailaConfigHandler config)
+    {
+        TileMimic mimic = (TileMimic) accessor.getTileEntity();
+        return mimic != null && mimic.getStackInSlot(0) != null ? mimic.getStackInSlot(0) : new ItemStack(ModBlocks.mimic);
+    }
+
+    @Override
+    public List<String> getWailaHead(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config)
+    {
+        return null;
+    }
+
+    @Override
+    public List<String> getWailaBody(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config)
+    {
+        return null;
+    }
+
+    @Override
+    public List<String> getWailaTail(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config)
+    {
+        return null;
+    }
+
+    @Override
+    public NBTTagCompound getNBTData(EntityPlayerMP player, TileEntity te, NBTTagCompound tag, World world, BlockPos pos)
+    {
+        return null;
+    }
+}
