3d372d18e3631fe5d1a6f08637dea6b2429c6ac3
WayofTime <wtime@live.ca>
2016-08-31 07:27:11 -0400

    Removed the Master Routing Node GUI.

--- src/main/java/WayofTime/bloodmagic/block/BlockMasterRoutingNode.java
+++ src/main/java/WayofTime/bloodmagic/block/BlockMasterRoutingNode.java
@@ -1,12 +1,6 @@
 package WayofTime.bloodmagic.block;
 
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumHand;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import WayofTime.bloodmagic.BloodMagic;
 import WayofTime.bloodmagic.api.Constants;
@@ -31,14 +25,14 @@ public class BlockMasterRoutingNode extends BlockRoutingNode
         return new TileMasterRoutingNode();
     }
 
-    @Override
-    public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ)
-    {
-        if (world.getTileEntity(pos) instanceof TileMasterRoutingNode)
-        {
-            player.openGui(BloodMagic.instance, Constants.Gui.MASTER_ROUTING_NODE_GUI, world, pos.getX(), pos.getY(), pos.getZ());
-        }
-
-        return true;
-    }
+//    @Override
+//    public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ)
+//    {
+//        if (world.getTileEntity(pos) instanceof TileMasterRoutingNode)
+//        {
+//            player.openGui(BloodMagic.instance, Constants.Gui.MASTER_ROUTING_NODE_GUI, world, pos.getX(), pos.getY(), pos.getZ());
+//        }
+//
+//        return true;
+//    }
 }
