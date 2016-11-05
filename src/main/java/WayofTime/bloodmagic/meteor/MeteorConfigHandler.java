d1e6076ccab57b82fafc81aebbfbf0ae16705fca
WayofTime <wtime@live.ca>
2016-11-05 19:29:03 -0400

    Added some more meteors - one that uses a gold block, another that uses a diamond block.

--- src/main/java/WayofTime/bloodmagic/meteor/MeteorConfigHandler.java
+++ src/main/java/WayofTime/bloodmagic/meteor/MeteorConfigHandler.java
@@ -107,9 +107,40 @@ public class MeteorConfigHandler
         ironMeteorList.add(new MeteorComponent(100, "oreRedstone"));
         Meteor ironMeteor = new Meteor(new ItemStack(Blocks.IRON_BLOCK), ironMeteorList, 15, 5, 1000);
         ironMeteor.setVersion(2);
+        // Gold
+        List<MeteorComponent> goldMeteorList = new ArrayList<MeteorComponent>();
+        goldMeteorList.add(new MeteorComponent(200, "oreIron"));
+        goldMeteorList.add(new MeteorComponent(100, "oreCopper"));
+        goldMeteorList.add(new MeteorComponent(60, "oreTin"));
+        goldMeteorList.add(new MeteorComponent(100, "oreGold"));
+        goldMeteorList.add(new MeteorComponent(120, "oreLapis"));
+        goldMeteorList.add(new MeteorComponent(30, "oreDiamond"));
+        goldMeteorList.add(new MeteorComponent(20, "oreEmerald"));
+        goldMeteorList.add(new MeteorComponent(20, "oreCoal"));
+
+        Meteor goldMeteor = new Meteor(new ItemStack(Blocks.GOLD_BLOCK), goldMeteorList, 18, 6, 1000);
+        goldMeteor.setVersion(3);
+
+        List<MeteorComponent> diamondMeteorList = new ArrayList<MeteorComponent>();
+        diamondMeteorList.add(new MeteorComponent(50, "oreIron"));
+        diamondMeteorList.add(new MeteorComponent(100, "oreGold"));
+        diamondMeteorList.add(new MeteorComponent(10, "oreLapis"));
+        diamondMeteorList.add(new MeteorComponent(250, "oreDiamond"));
+        diamondMeteorList.add(new MeteorComponent(180, "oreEmerald"));
+        diamondMeteorList.add(new MeteorComponent(50, "oreRedstone"));
+        diamondMeteorList.add(new MeteorComponent(400, "oreCoal"));
+
+        Meteor diamondMeteor = new Meteor(new ItemStack(Blocks.DIAMOND_BLOCK), diamondMeteorList, 10, 3, 1000);
+        goldMeteor.setVersion(3);
+
         holders.add(Pair.of("IronMeteor", ironMeteor));
         DEFAULT_METEORS.put("IronMeteor", ironMeteor);
 
+        holders.add(Pair.of("GoldMeteor", goldMeteor));
+        DEFAULT_METEORS.put("GoldMeteor", goldMeteor);
+
+        holders.add(Pair.of("DiamondMeteor", diamondMeteor));
+        DEFAULT_METEORS.put("DiamondMeteor", diamondMeteor);
         return holders;
     }
 }
