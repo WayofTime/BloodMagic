128b3d29b069066fa9ada274349a95a1612510de
Nicholas Ignoffo <speedynutty68@gmail.com>
2017-01-01 21:50:35 -0800

    Lowercase modid

--- src/main/java/WayofTime/bloodmagic/api/Constants.java
+++ src/main/java/WayofTime/bloodmagic/api/Constants.java
@@ -132,11 +132,11 @@ public class Constants
 
     public static class Mod
     {
-        public static final String MODID = "BloodMagic";
+        public static final String MODID = "bloodmagic";
         public static final String DOMAIN = MODID.toLowerCase(Locale.ENGLISH) + ":";
         public static final String NAME = "Blood Magic: Alchemical Wizardry";
         public static final String VERSION = "@VERSION@";
-        public static final String DEPEND = "required-after:Forge@[12.16.0.1840,);after:JEI@[2.23.0,);required-after:guideapi;";
+        public static final String DEPEND = "required-after:guideapi;";
     }
 
     public static final class Gui
