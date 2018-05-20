bd482663b46ea195fd948e43e99b3e92bd30cd43
Tobias Gremeyer <Iorce@users.noreply.github.com>
2018-05-20 02:05:51 +0200

    Fixed downgrade level inconsistency

--- src/main/java/WayofTime/bloodmagic/livingArmour/downgrade/LivingArmourUpgradeDisoriented.java
+++ src/main/java/WayofTime/bloodmagic/livingArmour/downgrade/LivingArmourUpgradeDisoriented.java
@@ -9,7 +9,7 @@ import net.minecraft.world.World;
 
 public class LivingArmourUpgradeDisoriented extends LivingArmourUpgrade {
     public static final int[] costs = new int[]{-10, -20, -30, -40, -70, -80, -100, -140, -180, -220};
-    public static final double[] chance = new double[]{0.001, 0.002, 0.003, 0.004, 0.005, 0.006, 0.008, 0.0010, 0.0012, 0.014};
+    public static final double[] chance = new double[]{0.001, 0.002, 0.003, 0.004, 0.005, 0.006, 0.008, 0.010, 0.012, 0.014};
 
     public LivingArmourUpgradeDisoriented(int level) {
         super(level);
