958223aa73ef75bdf0d6296e5c4f8ac1c0902713
Tobias <angryAEon@icloud.com>
2018-07-24 08:15:42 +0200

    Update BaseCompressionHandler.java

--- src/main/java/WayofTime/bloodmagic/compress/BaseCompressionHandler.java
+++ src/main/java/WayofTime/bloodmagic/compress/BaseCompressionHandler.java
@@ -50,7 +50,7 @@ public class BaseCompressionHandler extends CompressionHandler {
         for (ItemStack invStack : inv) {
             i++;
 
-            if (invStack.isEmpty() || invStack.equals(ItemStack.EMPTY) || invStack == ItemStack.EMPTY) {
+            if (invStack.isEmpty()) {
                 continue;
             }
 
