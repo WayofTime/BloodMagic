0ed4f1d977f72ee1cfa38b4de08383f0f4283586
iorce <angryaeon@icloud.com>
2019-09-04 17:12:41 +0200

    Activation Crystals can now activate portals!

--- src/main/java/WayofTime/bloodmagic/item/ItemActivationCrystal.java
+++ src/main/java/WayofTime/bloodmagic/item/ItemActivationCrystal.java
@@ -1,13 +1,21 @@
 package WayofTime.bloodmagic.item;
 
 import WayofTime.bloodmagic.BloodMagic;
-import WayofTime.bloodmagic.core.data.Binding;
-import WayofTime.bloodmagic.iface.IBindable;
 import WayofTime.bloodmagic.core.RegistrarBloodMagicItems;
+import WayofTime.bloodmagic.core.data.Binding;
+import WayofTime.bloodmagic.core.data.SoulTicket;
+import WayofTime.bloodmagic.iface.IBindable;
 import WayofTime.bloodmagic.item.types.ISubItem;
+import WayofTime.bloodmagic.util.helper.NetworkHelper;
 import WayofTime.bloodmagic.util.helper.TextHelper;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -42,6 +50,16 @@ public class ItemActivationCrystal extends ItemEnum.Variant<ItemActivationCrysta
 
     public int getCrystalLevel(ItemStack stack) {
         return stack.getItemDamage() > 1 ? Integer.MAX_VALUE : stack.getItemDamage() + 1;
+    }
+
+    @Override
+    public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        if (worldIn.getBlockState(pos).getBlock().equals(Blocks.OBSIDIAN) && Blocks.PORTAL.trySpawnPortal(worldIn, pos.offset(facing))) {
+            if (NetworkHelper.getSoulNetwork(player).syphonAndDamage(player, SoulTicket.item(player.getHeldItem(hand), 100)).isSuccess())
+                return EnumActionResult.SUCCESS;
+            else return EnumActionResult.FAIL;
+        }
+        return super.onItemUse(player, worldIn, pos, hand, facing, hitX, hitY, hitZ);
     }
 
     public enum CrystalType implements ISubItem {
