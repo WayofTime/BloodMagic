7fd10d0bdad694f35ef1a9f76a0dd60159dd1fe1
Jared <jaredlll08@gmail.com>
2020-12-22 00:17:48 +0200

    Register the recipe types with the correct name

--- src/main/java/wayoftime/bloodmagic/common/recipe/BloodMagicRecipeType.java
+++ src/main/java/wayoftime/bloodmagic/common/recipe/BloodMagicRecipeType.java
@@ -1,6 +1,7 @@
 package wayoftime.bloodmagic.common.recipe;
 
 import net.minecraft.item.crafting.IRecipeType;
+import wayoftime.bloodmagic.BloodMagic;
 import wayoftime.bloodmagic.recipe.RecipeARC;
 import wayoftime.bloodmagic.recipe.RecipeAlchemyArray;
 import wayoftime.bloodmagic.recipe.RecipeAlchemyTable;
@@ -9,9 +10,9 @@ import wayoftime.bloodmagic.recipe.RecipeTartaricForge;
 
 public class BloodMagicRecipeType
 {
-	public static final IRecipeType<RecipeBloodAltar> ALTAR = IRecipeType.register("altar");
-	public static final IRecipeType<RecipeAlchemyArray> ARRAY = IRecipeType.register("array");
-	public static final IRecipeType<RecipeTartaricForge> TARTARICFORGE = IRecipeType.register("soulforge");
-	public static final IRecipeType<RecipeARC> ARC = IRecipeType.register("arc");
-	public static final IRecipeType<RecipeAlchemyTable> ALCHEMYTABLE = IRecipeType.register("alchemytable");
+	public static final IRecipeType<RecipeBloodAltar> ALTAR = IRecipeType.register(BloodMagic.MODID + ":altar");
+	public static final IRecipeType<RecipeAlchemyArray> ARRAY = IRecipeType.register(BloodMagic.MODID + ":array");
+	public static final IRecipeType<RecipeTartaricForge> TARTARICFORGE = IRecipeType.register(BloodMagic.MODID + ":soulforge");
+	public static final IRecipeType<RecipeARC> ARC = IRecipeType.register(BloodMagic.MODID + ":arc");
+	public static final IRecipeType<RecipeAlchemyTable> ALCHEMYTABLE = IRecipeType.register(BloodMagic.MODID + ":alchemytable");
 }
