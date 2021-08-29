9c1acb20a28f2e1cf9bce11b67f68536b018efa4
damned <62070968+damned-me@users.noreply.github.com>
2021-08-29 21:29:39 +0200

    Improve BloodstoneBlock
    `wayoftime.bloodmagic.common.block.BloodMagicBlocks` register
    `wayoftime.bloodmagic.BloodstoneBlock` instead of `net.minecraft.block.Block`
    with equals properties.
     Changes to be committed:
    	modified:   src/main/java/wayoftime/bloodmagic/common/block/BloodMagicBlocks.java
    	modified:   src/main/java/wayoftime/bloodmagic/common/block/BloodstoneBlock.java

--- src/main/java/wayoftime/bloodmagic/common/block/BloodMagicBlocks.java
+++ src/main/java/wayoftime/bloodmagic/common/block/BloodMagicBlocks.java
@@ -79,8 +79,8 @@ public class BloodMagicBlocks
 	public static final RegistryObject<Block> DUSK_RITUAL_STONE = BLOCKS.register("duskritualstone", () -> new BlockRitualStone(EnumRuneType.DUSK));
 	public static final RegistryObject<Block> DAWN_RITUAL_STONE = BLOCKS.register("lightritualstone", () -> new BlockRitualStone(EnumRuneType.DAWN));
 
-	public static final RegistryObject<Block> BLOODSTONE = BASICBLOCKS.register("largebloodstonebrick", () -> new Block(Properties.create(Material.ROCK).hardnessAndResistance(2.0F, 5.0F).sound(SoundType.STONE).harvestTool(ToolType.PICKAXE).harvestLevel(1)));
-	public static final RegistryObject<Block> BLOODSTONE_BRICK = BASICBLOCKS.register("bloodstonebrick", () -> new Block(Properties.create(Material.ROCK).hardnessAndResistance(2.0F, 5.0F).sound(SoundType.STONE).harvestTool(ToolType.PICKAXE).harvestLevel(1)));
+	public static final RegistryObject<Block> BLOODSTONE = BASICBLOCKS.register("largebloodstonebrick", () -> new BloodstoneBlock());
+	public static final RegistryObject<Block> BLOODSTONE_BRICK = BASICBLOCKS.register("bloodstonebrick", () -> new BloodstoneBlock());
 
 	public static final RegistryObject<Block> MASTER_RITUAL_STONE = BASICBLOCKS.register("masterritualstone", () -> new BlockMasterRitualStone(false));
 
