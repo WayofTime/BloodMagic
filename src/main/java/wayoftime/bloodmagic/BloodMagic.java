9f6663b895d5f7429c4d019b4c9d9fd2ca48d5e1
VT-14 <GamingVT14@Gmail.com>
2021-09-12 14:50:37 -0400

    Fix Blood Elytra Render Crashing Servers on Startup
    ...I really should have caught that before the initial PR.

--- src/main/java/wayoftime/bloodmagic/BloodMagic.java
+++ src/main/java/wayoftime/bloodmagic/BloodMagic.java
@@ -19,6 +19,8 @@ import net.minecraft.item.crafting.SpecialRecipeSerializer;
 import net.minecraft.potion.Effect;
 import net.minecraft.tileentity.TileEntityType;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.ColorHandlerEvent;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoaderRegistry;
@@ -327,7 +329,12 @@ public class BloodMagic
 		new BloodMagicKeyHandler();
 //		IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
 //		
+		bloodElytraRender();
+	}
 
+	@OnlyIn(Dist.CLIENT)
+	private void bloodElytraRender()
+	{
 		Map<String, PlayerRenderer> skinMap = Minecraft.getInstance().getRenderManager().getSkinMap();
 		PlayerRenderer render;
 		render = skinMap.get("default");
