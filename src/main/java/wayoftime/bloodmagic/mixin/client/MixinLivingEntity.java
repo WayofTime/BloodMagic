77ee22328434f50f87b4b7b4b6524028d98eb599
WayofTime <WayofTime@users.noreply.github.com>
2022-06-19 10:55:30 -0400

    Fixed issue with Spectral Sight on server

--- src/main/java/wayoftime/bloodmagic/mixin/client/MixinLivingEntity.java
+++ src/main/java/wayoftime/bloodmagic/mixin/client/MixinLivingEntity.java
@@ -1,6 +1,7 @@
 package wayoftime.bloodmagic.mixin.client;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -8,6 +9,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.potion.Effect;
+import net.minecraft.potion.Effects;
 import wayoftime.bloodmagic.potion.BloodMagicPotions;
 
 @Mixin(LivingEntity.class)
@@ -22,6 +25,8 @@ public class MixinLivingEntity extends MixinEntity
 			return;
 		}
 
+		boolean success = false;
+
 		if (player.isPotionActive(BloodMagicPotions.SPECTRAL_SIGHT))
 		{
 			double distance = (player.getActivePotionEffect(BloodMagicPotions.SPECTRAL_SIGHT).getAmplifier() * 32 + 24);
@@ -32,9 +37,24 @@ public class MixinLivingEntity extends MixinEntity
 					if (!this.getFlag(6))
 					{
 						this.setFlag(6, true);
+						this.glowing = true;
 					}
 				}
+
+				success = true;
 			}
 		}
+
+		if (!success && this.glowing && !this.isPotionActive(Effects.GLOWING))
+		{
+			this.setFlag(6, false);
+			this.glowing = false;
+		}
+	}
+
+	@Shadow
+	public boolean isPotionActive(Effect potionIn)
+	{
+		throw new IllegalStateException("Mixin failed to shadow isPotionActive()");
 	}
 }
