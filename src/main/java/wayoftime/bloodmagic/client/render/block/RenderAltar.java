e1e3eae3984505e3fb4a00851d0000db32ce7670
mystchonky <dev.darshanphaldesai@gmail.com>
2023-08-08 00:38:58 +0530

    update client renderers for blocks

--- src/main/java/wayoftime/bloodmagic/client/render/block/RenderAltar.java
+++ src/main/java/wayoftime/bloodmagic/client/render/block/RenderAltar.java
@@ -2,12 +2,11 @@ package wayoftime.bloodmagic.client.render.block;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
-import com.mojang.math.Vector3f;
 
+import com.mojang.math.Axis;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.Sheets;
-import net.minecraft.client.renderer.block.model.ItemTransforms;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.entity.ItemRenderer;
@@ -16,6 +15,7 @@ import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.material.Fluid;
 import net.minecraftforge.client.extensions.common.IClientFluidTypeExtensions;
@@ -88,11 +88,11 @@ public class RenderAltar implements BlockEntityRenderer<TileAltar>
 
 			float rotation = (float) (720.0 * (System.currentTimeMillis() & 0x3FFFL) / 0x3FFFL);
 
-			matrixStack.mulPose(Vector3f.YP.rotationDegrees(rotation));
+			matrixStack.mulPose(Axis.YP.rotationDegrees(rotation));
 			matrixStack.scale(0.5F, 0.5F, 0.5F);
 //			Lighting.turnBackOn();
 			BakedModel ibakedmodel = itemRenderer.getModel(stack, tileAltar.getLevel(), (LivingEntity) null, 1);
-			itemRenderer.render(stack, ItemTransforms.TransformType.FIXED, true, matrixStack, buffer, combinedLightIn, combinedOverlayIn, ibakedmodel); // renderItem
+			itemRenderer.render(stack, ItemDisplayContext.FIXED, true, matrixStack, buffer, combinedLightIn, combinedOverlayIn, ibakedmodel); // renderItem
 
 //			int k = this.getLightVal(p_115076_, 15728880, p_115081_);
 //            p_115079_.scale(0.5F, 0.5F, 0.5F);
