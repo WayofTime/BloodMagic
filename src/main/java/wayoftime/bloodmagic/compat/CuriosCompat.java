131015ff6ab8326182b9cdd2c7d5f3dcc0b1724b
VT-14 <GamingVT14@gmail.com>
2023-09-28 10:53:33 -0400

    Curios Update Continued.
    Bump Curios to the latest available 1.20.1 version.
    
    Transient modifiers seem to handle relogs better, but is still very buggy.

--- src/main/java/wayoftime/bloodmagic/compat/CuriosCompat.java
+++ src/main/java/wayoftime/bloodmagic/compat/CuriosCompat.java
@@ -4,23 +4,14 @@ import net.minecraft.core.NonNullList;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.common.util.LazyOptional;
-import net.minecraftforge.fml.InterModComms;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.IItemHandlerModifiable;
 import top.theillusivec4.curios.api.CuriosApi;
-import top.theillusivec4.curios.api.type.capability.ICuriosItemHandler;
 import top.theillusivec4.curios.api.type.inventory.ICurioStacksHandler;
-import top.theillusivec4.curios.api.type.util.ISlotHelper;
-import wayoftime.bloodmagic.BloodMagic;
 import wayoftime.bloodmagic.core.LivingArmorRegistrar;
 import wayoftime.bloodmagic.core.living.LivingStats;
 import wayoftime.bloodmagic.core.living.LivingUtil;
 import wayoftime.bloodmagic.impl.BloodMagicAPI;
-
 import java.util.Map;
-import java.util.UUID;
 
 public class CuriosCompat
 {
@@ -48,19 +39,16 @@ public class CuriosCompat
 		{
 			Double amount = 0.0;
 			LivingStats stats = LivingStats.fromPlayer(player);
-			int curiosLevel = stats != null ? stats.getLevel(LivingArmorRegistrar.UPGRADE_CURIOS_SOCKET.get().getKey())
-					: 0;
-
+			int curiosLevel = (stats != null) ? stats.getLevel(LivingArmorRegistrar.UPGRADE_CURIOS_SOCKET.get().getKey()) : 0;
 			if (curiosLevel != 0)
 			{
 				amount = Double.valueOf(LivingArmorRegistrar.UPGRADE_CURIOS_SOCKET.get().getBonusValue("slots", curiosLevel).intValue());
 			}
-			System.out.println("VT Test Code: amount = " + amount);
-			curiosMap.get("living_armour_socket").addPermanentModifier(new AttributeModifier(player.getUUID(), "legacy", amount, AttributeModifier.Operation.ADDITION));
+			curiosMap.get("living_armour_socket").addTransientModifier(new AttributeModifier(player.getUUID(), "legacy", amount, AttributeModifier.Operation.ADDITION));
 			return curiosLevel;
 		} else
 		{
-			curiosMap.get("living_armour_socket").addPermanentModifier(new AttributeModifier(player.getUUID(), "legacy", 0.0, AttributeModifier.Operation.ADDITION));
+			curiosMap.get("living_armour_socket").addTransientModifier(new AttributeModifier(player.getUUID(), "legacy", 0.0, AttributeModifier.Operation.ADDITION));
 			return 0;
 		}
 	}
