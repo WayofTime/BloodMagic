7390db99922ce32507d5c2c3ddaf7d52728ab580
stellanera <stellanera98@gmail.com>
2025-10-25 06:42:45 +0200

    messed up during rebase

--- src/main/java/wayoftime/bloodmagic/common/tags/BloodMagicTags.java
+++ src/main/java/wayoftime/bloodmagic/common/tags/BloodMagicTags.java
@@ -30,12 +30,16 @@ public class BloodMagicTags
     public static final TagKey<Block> TELEPOSE_BLOCK_BLACKLIST = BlockTags.create(BloodMagic.rl("telepose_blacklist"));
     public static final TagKey<EntityType<?>> TELEPOSE_BLACKLIST = TagKey.create(Registries.ENTITY_TYPE, BloodMagic.rl("telepose_blacklist"));
 
+    public static final TagKey<Item> DUNGEON_RAW = ItemTags.create(BloodMagic.rl("dungeon_raw"));
+    public static final TagKey<Item> DUNGEON_CORROSIVE = ItemTags.create(BloodMagic.rl("dungeon_corrosive"));
+    public static final TagKey<Item> DUNGEON_DESTRUCTIVE = ItemTags.create(BloodMagic.rl("dungeon_destructive"));
+    public static final TagKey<Item> DUNGEON_STEADFAST = ItemTags.create(BloodMagic.rl("dungeon_steadfast"));
+    public static final TagKey<Item> DUNGEON_VENGEFUL = ItemTags.create(BloodMagic.rl("dungeon_vengeful"));
+
 	// Storage Blocks
 	public static final TagKey<Block> BLOCK_QUARTZ = getForgeBlockStorageTag("quartz");
 	public static final TagKey<Block> BLOCK_URANIUM = getForgeBlockStorageTag("uranium");
 	public static final TagKey<Block> BLOCK_SLAG = getForgeBlockStorageTag("slag");
-	public static final TagKey<Block> BLOCK_FLUIX = BlockTags.create(new ResourceLocation("appliedenergistics2:fluix_block"));
-	public static final TagKey<Block> BLOCK_SKY_STONE = BlockTags.create(new ResourceLocation("appliedenergistics2:sky_stone_block"));
     public static final TagKey<Item> BLOCK_HELLFORGED = ItemTags.create(new ResourceLocation("forge:storage_blocks/hellforged"));
 
 	// Ores
