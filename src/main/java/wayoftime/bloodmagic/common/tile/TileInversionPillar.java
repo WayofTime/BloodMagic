2d31aa94170a85eaaee4505a413dbc523b0728b2
stellanera <stellanera98@gmail.com>
2025-11-03 18:11:08 +0100

    sorta working minus teleport location

--- src/main/java/wayoftime/bloodmagic/common/tile/TileInversionPillar.java
+++ src/main/java/wayoftime/bloodmagic/common/tile/TileInversionPillar.java
@@ -3,6 +3,7 @@ package wayoftime.bloodmagic.common.tile;
 import net.minecraft.commands.CommandSource;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.nbt.CompoundTag;
@@ -11,14 +12,27 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.storage.LevelData;
 import net.minecraft.world.phys.Vec2;
 import net.minecraft.world.phys.Vec3;
+import wayoftime.bloodmagic.BloodMagic;
+import wayoftime.bloodmagic.common.block.BlockInversionPillarEnd;
+import wayoftime.bloodmagic.common.block.BloodMagicBlocks;
+import wayoftime.bloodmagic.common.block.type.PillarCapType;
 import wayoftime.bloodmagic.common.tile.base.TileBase;
+import wayoftime.bloodmagic.structures.DungeonRoomLoader;
+import wayoftime.bloodmagic.structures.DungeonRoomRegistry;
+import wayoftime.bloodmagic.structures.DungeonSynthesizer;
+import wayoftime.bloodmagic.structures.ModDungeons;
+import wayoftime.bloodmagic.util.BMLog;
+import wayoftime.bloodmagic.util.ChatUtil;
 import wayoftime.bloodmagic.util.Constants;
 
 import java.util.UUID;
@@ -43,6 +57,10 @@ public class TileInversionPillar extends TileBase implements CommandSource
 		this.destinationKey = destinationWorld.dimension();
 		this.teleportPos = destinationPos;
 	}
+
+    public boolean hasDestination() {
+        return teleportPos != BlockPos.ZERO;
+    }
 
 	@Override
 	public void deserialize(CompoundTag tag)
@@ -77,11 +95,46 @@ public class TileInversionPillar extends TileBase implements CommandSource
 		return tag;
 	}
 
-	public void handlePlayerInteraction(Player player)
+	public void handlePlayerInteraction(ServerPlayer player)
 	{
-//		RegistryKey<World> key = RegistryKey.getOrCreateKey(Registry.WORLD_KEY, BloodMagic.rl("dungeon"));
+        BlockPos pillarPos = this.getBlockPos();
+        BlockPos controllerPos = pillarPos.above(2);
+        // this could probably be cheated relatively easily but I dont really know how to do it better. Even adding more blocks doesnt prevent a sufficiently motivated person from just moving it.
+        // potentially solvable by making seals not drop anything and making them immovable by pistons
+        boolean isEndless = level.getBlockState(pillarPos.below().north(10)).is(BloodMagicBlocks.DUNGEON_SEAL.get());
+
+        // no controller block above pillar? probably used to be the ritual side so skipping check
+        if (level.getBlockState(controllerPos).is(BloodMagicBlocks.DUNGEON_CONTROLLER.get())) {
+            if (level.getBlockEntity(controllerPos) instanceof TileDungeonController controller) {
+                if (controller.dungeon.availableDoorMasterMap.isEmpty()) {
+                    DungeonSynthesizer dungeon = new DungeonSynthesizer();
+                    dungeon.generateInitialRoom(BloodMagic.rl(isEndless ? "room_pools/entrances/standard_dungeon_entrances" : "room_pools/entrances/mini_dungeon_entrances"), level.random, (ServerLevel) level, controllerPos);
+
+                    // dungeon nbt file has regular pillars here, fix that
+                    level.setBlockAndUpdate(pillarPos, BloodMagicBlocks.INVERSION_PILLAR.get().defaultBlockState());
+                    level.setBlockAndUpdate(pillarPos.relative(Direction.DOWN), BloodMagicBlocks.INVERSION_PILLAR_CAP.get().defaultBlockState().setValue(BlockInversionPillarEnd.TYPE, PillarCapType.BOTTOM));
+                    level.setBlockAndUpdate(pillarPos.relative(Direction.UP), BloodMagicBlocks.INVERSION_PILLAR_CAP.get().defaultBlockState().setValue(BlockInversionPillarEnd.TYPE, PillarCapType.TOP));
+
+                    ChatUtil.sendNoSpam(player, Component.translatable("chat.bloodmagic.dungeon.controller_fix"));
+
+                    // no teleporty, bad teleporty
+                    return;
+                }
+            } else {
+                BMLog.DEFAULT.warn("No controller BE (but block is there) found at {} while attempting to fix already broken dungeon. I pray we never see this message in a log");
+            }
+        }
+
 		if (teleportPos.equals(BlockPos.ZERO))
 		{
+            ChatUtil.sendNoSpam(player, Component.translatable("chat.bloodmagic.dungeon.position_fix"));
+            ResourceKey<Level> dimKey = player.getRespawnDimension();
+            BlockPos respawnPos = player.getRespawnPosition();
+            if (respawnPos == null) {
+                LevelData data = level.getServer().getLevel(dimKey).getLevelData();
+                respawnPos = new BlockPos(data.getXSpawn(), data.getYSpawn(), data.getZSpawn());
+            }
+            teleportPlayerToLocation((ServerLevel) level, player, player.getRespawnDimension(), respawnPos);
 			return;
 		}
 
