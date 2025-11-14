62cbc700f4d0dae3c712a289299e51e0d47406cb
stellanera <stellanera98@gmail.com>
2025-11-14 21:20:36 +0100

    spike death message

--- src/main/java/wayoftime/bloodmagic/common/data/GeneratorLanguage.java
+++ src/main/java/wayoftime/bloodmagic/common/data/GeneratorLanguage.java
@@ -28,6 +28,8 @@ public class GeneratorLanguage extends LanguageProvider
 		//add("chat.bloodmagic.damageSource", "%s's soul became too weak");
 		add("death.attack.sacrifice", "%1$s sacrificed a bit too much LP");
 		add("death.attack.ritual", "%1$s died in a Well of Suffering");
+        add("death.attack.spike", "%1$s was impaled on spikes");
+        add("death.attack.spike.player", "%1$s was impaled on spikes while fighting %2$s");
 
 		// Tile Entitites
 		add("tile.bloodmagic.soulforge.name", "Hellfire Forge");
