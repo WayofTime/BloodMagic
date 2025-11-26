005c29dbc1b410a3d93dc2617239f1955fffa760
stellanera <stellanera98@gmail.com>
2025-11-26 01:16:16 +0100

    frozen lake sigil

--- src/main/java/wayoftime/bloodmagic/common/item/BloodMagicItems.java
+++ src/main/java/wayoftime/bloodmagic/common/item/BloodMagicItems.java
@@ -217,6 +217,7 @@ public class BloodMagicItems
 	public static final RegistryObject<Item> REAGENT_HOLDING = BASICITEMS.register("reagentholding", () -> new ItemBase());
 	public static final RegistryObject<Item> REAGENT_SUPPRESSION = BASICITEMS.register("reagentsuppression", () -> new ItemBase());
 	public static final RegistryObject<Item> REAGENT_TELEPOSITION = BASICITEMS.register("reagentteleposition", () -> new ItemBase());
+    public static final RegistryObject<Item> REAGENT_FROST = BASICITEMS.register("reagentfrost", () -> new ItemBase());
 
 	// Tartaric Gems
 	public static final RegistryObject<Item> PETTY_GEM = ITEMS.register("soulgempetty", () -> new ItemSoulGem("petty", 64));
