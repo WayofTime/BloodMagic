30382de2fd11300b54e75f08a44216f0e5092e64
stellanera <stellanera98@gmail.com>
2025-12-20 01:13:16 +0100

    mixin stuff

--- src/main/java/wayoftime/bloodmagic/mixin/client/MixinLivingEntity.java
+++ src/main/java/wayoftime/bloodmagic/mixin/client/MixinLivingEntity.java
@@ -2,15 +2,23 @@ package wayoftime.bloodmagic.mixin.client;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.client.Minecraft;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import wayoftime.bloodmagic.potion.BloodMagicPotions;
 
 @Mixin(LivingEntity.class)
-public abstract class MixinLivingEntity extends MixinEntity
+public abstract class MixinLivingEntity extends Entity
 {
+    public MixinLivingEntity(EntityType<?> p_19870_, Level p_19871_) {
+        super(p_19870_, p_19871_);
+        throw new IllegalStateException("Instantiated MixinLivingEntity");
+    }
+
     @ModifyReturnValue(
             method = "isCurrentlyGlowing",
             at = @At(value = "RETURN")
