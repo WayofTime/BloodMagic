4a41ecbfb8b20c12053e72636b37eda466c27b29
wrincewind <Cott002367@yahoo.co.uk>
2026-05-02 19:02:48 +0100

    1.21.1 modopedia (#2180)
    * update gradle to include latest version of Modopedia. (note to self, check for Modopedia updates and update modopedia_version accordingly)
    
    * added generators for the modopedia book, slightly adjusted grouping in BMLanguageProvider.java and added headers
    
    * Updated the guide, fixed the colour and corrected some language issues
    
    * Updated the rendering of the alchemy table - now it's transparent. [unfortunately this means it's occasionally *too* transparent... you win sme, you lose some)

--- src/main/java/wayoftime/bloodmagic/common/block/BMBlocks.java
+++ src/main/java/wayoftime/bloodmagic/common/block/BMBlocks.java
@@ -46,8 +46,8 @@ public class BMBlocks {
     public static final BlockWithItemHolder<ImperfectRitualBlock, BlockItem> IMPERFECT_RITUAL_BLOCK = BASIC_REG.register("ritual_stone_imperfect", ImperfectRitualBlock::new);
 
     private static final BlockBehaviour.Properties rune_properties = BlockBehaviour.Properties.of().strength(2.0F, 5.0F).sound(SoundType.STONE).requiresCorrectToolForDrops();
-    private static final ItemLore save_decoration = new ItemLore(List.of(BlockEntityHelper.translatableHover("tooltip.bloodmagic.save_for_decoration").withStyle(ChatFormatting.ITALIC)));
-    private static final Item.Properties decoration_item_properties = new Item.Properties().component(DataComponents.LORE, save_decoration);
+    private static final ItemLore safe_decoration = new ItemLore(List.of(BlockEntityHelper.translatableHover("tooltip.bloodmagic.safe_for_decoration").withStyle(ChatFormatting.ITALIC)));
+    private static final Item.Properties decoration_item_properties = new Item.Properties().component(DataComponents.LORE, safe_decoration);
 
     public static final BlockWithItemHolder<Block, BlockItem> RUNE_BLANK = BASIC_REG.register("rune_blank", rune_properties, decoration_item_properties);
 
